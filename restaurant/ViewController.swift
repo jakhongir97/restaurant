c7cc73d31fd1f645ac9bb4191ae33536a0ce3116
Jakhongir Nematov <jahongirnematov@jahongirs-macbook-pro.local>
2018-03-10 13:18:46 +0500

    Initial Commit

---
+++ restaurant/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  restaurant
+//
+//  Created by Jahongir Nematov on 3/10/18.
+//  Copyright © 2018 Jahongir Nematov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
